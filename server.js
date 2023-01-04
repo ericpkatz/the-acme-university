6cdfac6a34ea04bd5f181e2d0040426e10793146
ericpkatz <ericpkatz@gmail.com>
2023-01-04 11:25:11 -0500

    fix port

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const server = http.createServer((req, res)=> {
 });
 
 const port = process.env.PORT || 3000;
-server.listen(3000, ()=> {
+server.listen(port, ()=> {
   console.log(`listening on port ${port}`);
 })
