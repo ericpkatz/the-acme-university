4cdf23aac65327ac9282dd84d8ca38ffc7ab0bf4
ericpkatz <ericpkatz@gmail.com>
2023-01-04 11:34:35 -0500

    more pages

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const http = require('http');
 const server = http.createServer((req, res)=> {
-  res.write('hello world');
-  res.end();
+  if(req.url === '/'){
+    res.write('Hello world');
+    res.end();
+  }
+  else if(req.url === '/foo'){
+    res.write('TBD foo page');
+    res.end();
+  }
+  else if(req.url === '/bar'){
+    res.write('TBD bar page');
+    res.end();
+  }
+  else if(req.url === '/bazz'){
+    res.write('TBD bazz page');
+    res.end();
+  }
+  else {
+    res.statusCode = 404;
+    res.write('This page doesnt exist!');
+    res.end();
+  }
 });
 
 const port = process.env.PORT || 3000;
